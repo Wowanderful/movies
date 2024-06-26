0cc7e646458b3396d8005a6d582fc12720c6696f
Vladimir Kravchenko <wowanderful@gmail.com>
2024-06-26 10:57:31 -0400

    functional components added

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,61 +1,123 @@
-import React from 'react';
+import React, {useState, } from 'react';
 
-class Search extends React.Component {
+//FUNCTION COMPONENTS REALIZATION
 
-    state = {
-        search: '', 
-        type: 'all,'
-    }
+function Search (props) {
 
-    handleFilter = (event) => {
-        this.setState(() => ({type: event.target.dataset.type}), () => {
-            this.props.searchMovies(this.state.search, this.state.type);
-        });
-    };
+    const {searchMovies = Function.prototype} = props;
 
-    handleKey = (event) => {
+    const [search, setSearch] = useState('');
+    const [type, setType] = useState('all');
+
+    const handleKey = (event) => {
         if(event.key === 'Enter') {
-            this.props.searchMovies(this.state.search, this.state.type)
+            searchMovies(search, type)
         }
+    }    
+    
+    const handleFilter = (event) => {
+        setType(event.target.dataset.type);
+        searchMovies(search, event.target.dataset.type);
     }
 
-    render() {
-        return (
-            <div className="row">
-                <div className="col s12">
-                    <div className="input-field">
-                        <input 
-                            placeholder="search" 
-                            id="search" type="search" 
-                            className="validate" 
-                            value={this.state.search}
-                            onChange={(e) => this.setState({search: e.target.value})}
-                            onKeyDown={this.handleKey}
-                        />
-                        <button className="btn btn-search" onClick={() => this.props.searchMovies(this.state.search, this.state.type)}>Saerch</button>
-                    </div>
-                    <div className="filters">
-                        <label>
-                            <input className="with-gap" name="type" type="radio" data-type="all" 
-                            onChange={this.handleFilter} checked={this.state.type === 'all'} />
-                            <span>All</span>
-                        </label>
-                        <label>
-                            <input className="with-gap" name="type" type="radio" data-type="movie" 
-                            onChange={this.handleFilter} checked={this.state.type === 'movie'}/>
-                            <span>Movies only</span>
-                        </label>
-                        <label>
-                            <input className="with-gap" name="type" type="radio" data-type="series" 
-                            onChange={this.handleFilter} checked={this.state.type === 'series'}/>
-                            <span>Series only</span>
-                        </label>
-                        
-                    </div>
+    return (
+        <div className="row">
+           <div className="col s12">
+                <div className="input-field">
+                    <input 
+                        placeholder="search" 
+                        id="search" type="search" 
+                        className="validate" 
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
+                        onKeyDown={handleKey}
+                    />
+                    <button className="btn btn-search" onClick={() => this.props.searchMovies(search, type)}>Search</button>
+                </div>
+                <div className="filters">
+                    <label>
+                        <input className="with-gap" name="type" type="radio" data-type="all" 
+                            onChange={handleFilter} checked={type === 'all'} />
+                        <span>All</span>
+                    </label>
+                    <label>
+                         <input className="with-gap" name="type" type="radio" data-type="movie" 
+                            onChange={handleFilter} checked={type === 'movie'}/>
+                        <span>Movies only</span>
+                    </label>
+                    <label>
+                        <input className="with-gap" name="type" type="radio" data-type="series" 
+                            onChange={handleFilter} checked={type === 'series'}/>
+                        <span>Series only</span>
+                    </label>
+                            
                 </div>
             </div>
-        )
-    }
+        </div>
+            )
+
 }
+
+
+
+//CLASS COMPONENT REALIZATION: 
+
+// class Search extends React.Component {
+
+//     state = {
+//         search: '', 
+//         type: 'all,'
+//     }
+
+//     handleFilter = (event) => {
+//         this.setState(() => ({type: event.target.dataset.type}), () => {
+//             this.props.searchMovies(this.state.search, this.state.type);
+//         });
+//     };
+
+//     handleKey = (event) => {
+//         if(event.key === 'Enter') {
+//             this.props.searchMovies(this.state.search, this.state.type)
+//         }
+//     }
+
+//     render() {
+//         return (
+//             <div className="row">
+//                 <div className="col s12">
+//                     <div className="input-field">
+//                         <input 
+//                             placeholder="search" 
+//                             id="search" type="search" 
+//                             className="validate" 
+//                             value={this.state.search}
+//                             onChange={(e) => this.setState({search: e.target.value})}
+//                             onKeyDown={this.handleKey}
+//                         />
+//                         <button className="btn btn-search" onClick={() => this.props.searchMovies(this.state.search, this.state.type)}>Saerch</button>
+//                     </div>
+//                     <div className="filters">
+//                         <label>
+//                             <input className="with-gap" name="type" type="radio" data-type="all" 
+//                             onChange={this.handleFilter} checked={this.state.type === 'all'} />
+//                             <span>All</span>
+//                         </label>
+//                         <label>
+//                             <input className="with-gap" name="type" type="radio" data-type="movie" 
+//                             onChange={this.handleFilter} checked={this.state.type === 'movie'}/>
+//                             <span>Movies only</span>
+//                         </label>
+//                         <label>
+//                             <input className="with-gap" name="type" type="radio" data-type="series" 
+//                             onChange={this.handleFilter} checked={this.state.type === 'series'}/>
+//                             <span>Series only</span>
+//                         </label>
+                        
+//                     </div>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
 
 export { Search }
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,45 +1,96 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Movies } from '../components/Movies';
 import { Preloader } from '../components/preloader';
 import { Search } from '../components/Search';
 
+//FUNCTIONAL COMPONENT REALIZATION
 
-class Main extends React.Component {
+function Main () {
 
-    state = {
-        movies: [],
-        loading: true,
-    }
+    const [movies, setMovies] = useState();
+    const [loading, setLoading] = useState(true);
 
-    componentDidMount () {
-        fetch('http://www.omdbapi.com/?i=tt3896198&apikey=74ea4022&s=matrix')
-            .then(response => response.json())
-            .then(data => this.setState({movies: data.Search, loading: false}))
-    }
-
-    searchMovies = (str, type) => {
-        this.setState({loading: true});
+    const searchMovies = (str, type) => {
+        setLoading(true);
         fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=74ea4022&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
             .then(response => response.json())
-            .then(data => this.setState({movies: data.Search, loading: false}))
+            .then((data) => {
+                setLoading(false);
+                setMovies(data.Search)
+            })
+            .catch((err) => {
+                console.log('error');
+                setLoading(false);
+            })
     }
 
-    render() {
-        const {movies, loading} = this.state;
+    useEffect(() => {
+        fetch('http://www.omdbapi.com/?i=tt3896198&apikey=74ea4022&s=matrix')
+            .then((response) => response.json())
+            .then((data) => {
+                setMovies(data.Search);
+                setLoading(false);
+            })
+            .catch((err) => {
+                console.log('error');
+                setLoading(false);
+            })
+        }, []
+    )
 
-        return (
+
+
+    return (
             
-            <main className = "container content"> 
+        <main className = "container content"> 
 
-            <Search searchMovies={this.searchMovies} />
+        <Search searchMovies={searchMovies} />
 
-            { loading ? <Preloader /> : <Movies movies={movies}/> 
+        { loading ? <Preloader /> : <Movies movies={movies}/> 
                 }
-            </main>
+        </main>
         )
-    }
+
 
 }
+
+//CLASS COMPONENT REALIZATION: 
+// class Main extends React.Component {
+
+//     state = {
+//         movies: [],
+//         loading: true,
+//     }
+
+//     componentDidMount () {
+//         fetch('http://www.omdbapi.com/?i=tt3896198&apikey=74ea4022&s=matrix')
+//             .then(response => response.json())
+//             .then(data => this.setState({movies: data.Search, loading: false}))
+//     }
+
+//     searchMovies = (str, type) => {
+//         this.setState({loading: true});
+//         fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=74ea4022&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
+//             .then(response => response.json())
+//             .then(data => this.setState({movies: data.Search, loading: false}))
+//     }
+
+//     render() {
+//         const {movies, loading} = this.state;
+
+//         return (
+            
+//             <main className = "container content"> 
+
+//             <Search searchMovies={this.searchMovies} />
+
+//             { loading ? <Preloader /> : <Movies movies={movies}/> 
+//                 }
+//             </main>
+//         )
+//     }
+
+// }
 
 
 export { Main }
